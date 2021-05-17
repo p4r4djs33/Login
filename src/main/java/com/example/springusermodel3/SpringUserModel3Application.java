68ecdd32524950de53e23a187afaf5d21fd813a2
Kien <tata.vutrungkien@gmail.com>
2021-05-17 14:41:24 +0700

    init

---
+++ src/main/java/com/example/springusermodel3/SpringUserModel3Application.java
@@ -0,0 +1,13 @@
+package com.example.springusermodel3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserModel3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUserModel3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springusermodel3/SpringUserModel3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springusermodel3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUserModel3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
